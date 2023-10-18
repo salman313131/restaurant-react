95bb83e1dcfcf08a2158ad723e99a6aa2e410118
salman <salmankhan.faze@gmail.com>
2023-10-18 12:18:52 +0530

    meals summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 import Header from './Components/Layout/Header'
+import Meals from "./Components/Meals/Meals";
 function App() {
   return (
    <Fragment>
       <Header />
+      <Meals />
    </Fragment>
   );
 }
